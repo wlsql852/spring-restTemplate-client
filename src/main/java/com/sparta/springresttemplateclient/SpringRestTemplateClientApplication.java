c3c609e8c110e64dc0338e959e95eccd4bd76bb1
wlsql852 <wlsql852@naver.com>
2024-08-26 15:10:00 +0900

    init commit

---
+++ src/main/java/com/sparta/springresttemplateclient/SpringRestTemplateClientApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springresttemplateclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTemplateClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestTemplateClientApplication.class, args);
+    }
+
+}
